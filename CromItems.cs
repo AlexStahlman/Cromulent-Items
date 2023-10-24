b1622a8f1a42b64fa92e4497ed4d84cdd383e1d7
Alexander James Stahlman <astahlman17@gmail.com>
2023-10-24 12:50:41 -0400

    ver 0.1.0

---
+++ CromItems.cs
@@ -0,0 +1,72 @@
+﻿using BepInEx;
+using R2API;
+using R2API.Utils;
+using RoR2;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using System.Collections.Generic;
+namespace CromulentItems
+{
+    //This is an example plugin that can be put in BepInEx/plugins/ExamplePlugin/ExamplePlugin.dll to test out.
+    //It's a small plugin that adds a relatively simple item to the game, and gives you that item whenever you press F2.
+
+    //This attribute specifies that we have a dependency on R2API, as we're using it to add our item to the game.
+    //You don't need this if you're not using R2API in your plugin, it's just to tell BepInEx to initialize R2API before this plugin so it's safe to use R2API.
+    [BepInDependency(R2API.R2API.PluginGUID)]
+
+    //This attribute is required, and lists metadata for your plugin.
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+
+    //We will be using 2 modules from R2API: ItemAPI to add our item and LanguageAPI to add our language tokens.
+    [R2APISubmoduleDependency(nameof(ItemAPI), nameof(LanguageAPI))]
+    public class CromItems : BaseUnityPlugin
+    {
+        //The Plugin GUID should be a unique ID for this plugin, which is human readable (as it is used in places like the config).
+        //If we see this PluginGUID as it is on thunderstore, we will deprecate this mod. Change the PluginAuthor and the PluginName !
+        public const string PluginGUID = PluginAuthor + "." + PluginName;
+        public const string PluginAuthor = "Spartan";
+        public const string PluginName = "CromulentItems";
+        public const string PluginVersion = "0.0.1";
+
+        public static BepInEx.Logging.ManualLogSource Log;
+        public GameObject multiShopPrefab;
+        public ItemTier[] itemTiers;
+        public static PluginInfo PInfo { get; private set; }
+
+        //The Awake() method is run at the very start when the game is initialized.
+        public void Awake()
+        {
+            //Init our logging class so that we can properly log for debugging
+            Log = Logger;
+
+            PInfo = Info;
+            Assets.Init();
+            JacobBlast.Init();
+
+            // This line of log will appear in the bepinex console when the Awake method is done.
+            Log.LogInfo("CromulentItems successfully loaded.");
+        }
+
+        //The Update() method is run on every frame of the game.
+        private void Update()
+        {
+            /*
+            // ONLY FOR TESTING
+            if (Input.GetKeyDown(KeyCode.Comma))
+            {
+                // //     //Get the player body to use a position:
+                var transform = PlayerCharacterMasterController.instances[0].master.GetBodyObject().transform;
+
+                // //     //And then drop our defined item in front of the player.
+
+                Log.LogInfo($"Player pressed comma. Spawning our custom item at coordinates {transform.position}");
+                //List<ItemDef> myItems = new() { JacobBlast.myItemDef };
+                // (ItemDef myItem in myItems)
+                //{
+                PickupDropletController.CreatePickupDroplet(PickupCatalog.FindPickupIndex(JacobBlast.myItemDef.itemIndex), transform.position, transform.forward * 20f);
+                //}
+            }
+            */
+        }
+    }
+}
---
+++ Items/JacobBlast.cs
@@ -0,0 +1,126 @@
+﻿using System.Threading;
+using System.Globalization;
+using System.Collections.Generic;
+using BepInEx;
+using R2API;
+using R2API.Utils;
+using RoR2.Orbs;
+using RoR2;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using System;
+
+namespace CromulentItems
+{
+    internal class JacobBlast
+    {
+
+
+        //We need our item definition to persist through our functions, and therefore make it a class field.
+        public static ItemDef myItemDef;
+
+        // Set luck amount in one location
+        public static float luckAmount = 10f;
+        public static float numItems = 0f;
+
+        // Set equipment cd in one location
+        public static float cooldownAmount = 1f - (30f * .01f);
+
+        internal static void Init()
+        {
+            //Generate the basic information for the item
+            CreateItem();
+
+            //Now let's turn the tokens we made into actual strings for the game:
+            AddTokens();
+
+            //You can add your own display rules here, where the first argument passed are the default display rules: the ones used when no specific display rules for a character are found.
+            //For this example, we are omitting them, as they are quite a pain to set up without tools like ItemDisplayPlacementHelper
+            var displayRules = new ItemDisplayRuleDict(null);
+
+            //Then finally add it to R2API
+            ItemAPI.Add(new CustomItem(myItemDef, displayRules));
+
+            // Initialize the hooks
+            hooks();
+        }
+
+        private static void CreateItem()
+        {
+            //First let's define our item
+            myItemDef = ScriptableObject.CreateInstance<ItemDef>();
+
+            // Language Tokens, check AddTokens() below.
+            myItemDef.name = "JacobBlast";
+            myItemDef.nameToken = "JacobBlast";
+            myItemDef.pickupToken = "JacobBlastItem";
+            myItemDef.descriptionToken = "JacobBlastDesc";
+            myItemDef.loreToken = "JacobBlastDesc";
+            myItemDef.tags = new ItemTag[1] { ItemTag.Utility };
+
+            //The tier determines what rarity the item is:
+            //Tier1=white, Tier2=green, Tier3=red, Lunar=Lunar, Boss=yellow,
+            //and finally NoTier is generally used for helper items, like the tonic affliction
+#pragma warning disable Publicizer001 // Accessing a member that was not originally public. Here we ignore this warning because with how this example is setup we are forced to do this
+            myItemDef._itemTierDef = Addressables.LoadAssetAsync<ItemTierDef>("RoR2/Base/Common/Tier3Def.asset").WaitForCompletion();
+#pragma warning restore Publicizer001
+            //myItemDef.pickupIconSprite = Resources.Load<Sprite>("Assets/CromulentItems/AssetsFolder/icons/JacobBluePNG");
+            var icon = Assets.icons.LoadAsset<Sprite>("JacobBluePNG");
+            myItemDef.pickupIconSprite = icon;
+            //myItemDef.pickupModelPrefab = Resources.Load<GameObject>("Assets/CromulentItems/AssetsFolder/prefabs/JacobCube");
+            var prefab = Assets.prefabs.LoadAsset<GameObject>("JacobCube");
+            myItemDef.pickupModelPrefab = prefab;
+            //Can remove determines if a shrine of order, or a printer can take this item, generally true, except for NoTier items.
+            myItemDef.canRemove = true;
+
+            //Hidden means that there will be no pickup notification
+            myItemDef.hidden = false;
+        }
+
+
+        private static void hooks()
+        {
+
+            On.RoR2.CharacterBody.OnInventoryChanged += (orig, self) =>
+            {
+                orig(self);
+            };
+
+            On.RoR2.GlobalEventManager.OnHitEnemy += (orig, self, damageInfo, victim) =>
+            {
+                orig(self, damageInfo, victim);
+                if (damageInfo.attacker && damageInfo.procCoefficient > 0)
+                {
+                    CharacterBody attackerCharacterBody = damageInfo.attacker.GetComponent<CharacterBody>();
+                    CharacterBody victimCharacterBody = victim.GetComponent<CharacterBody>();
+
+                    if (attackerCharacterBody?.inventory)
+                    {
+                        int inventoryCount = attackerCharacterBody.inventory.GetItemCount(myItemDef.itemIndex);
+                        if (inventoryCount > 0 && Util.CheckRoll(inventoryCount/2, attackerCharacterBody.master))
+                        {
+                            attackerCharacterBody.AddTimedBuff(RoR2Content.Buffs.TonicBuff, 2);
+                        }
+                    }
+                }
+
+            };
+        }
+
+        //This function adds the tokens from the item using LanguageAPI, the comments in here are a style guide, but is very opiniated. Make your own judgements!
+        private static void AddTokens()
+        {
+            //The Name should be self explanatory
+            LanguageAPI.Add("JacobBlast", "Jacob Blast");
+
+            //The Pickup is the short text that appears when you first pick this up. This text should be short and to the point, numbers are generally ommited.
+            LanguageAPI.Add("JacobBlastItem", "Chance to activate 'Tonic'");
+
+            //The Description is where you put the actual numbers and give an advanced description.
+            LanguageAPI.Add("JacobBlastDesc", " <style=cIsUtility>" + numItems/2 + "</style> chance to Activate a Tonic Effect.");
+
+            //The Lore is, well, flavor. You can write pretty much whatever you want here.
+            LanguageAPI.Add("JacobBlastLore", "Blue Liquid Amalgomation");
+        }
+    }
+}
